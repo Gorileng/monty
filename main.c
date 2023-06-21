7231a3f011d09e02b68a117a7108034434ef11f7
Gorileng <keneilwe963@gmail.com>
2023-06-21 16:54:13 -0700

    monty project

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - the monty code interpreter
-* @argc: the number of arguments
-* @argv: the monty file location
-* Return: 0 success
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
 */
 int main(int argc, char *argv[])
 {
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	bus.file = file;
 	if (!file)
 	{
-		fprintf(stderr, "Error: Cannot open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read_line > 0)
